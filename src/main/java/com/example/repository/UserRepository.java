aac1e8eb72dde6d26036fc3a4ad7a74fd5bfe5ee
bimapopo345 <bimapopo345@gmail.com>
2025-06-10 15:38:12 +0700

    Updated: Done User Profile & Admin Profile

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -19,4 +22,13 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     
     // Check username exist
     boolean existsByUsername(String username);
+    
+    // Delete by role with native query
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.role = :role")
+    void deleteByRole(@Param("role") String role);
+    
+    // Count total users by role
+    @Query("SELECT COUNT(u) FROM User u WHERE u.role = :role")
+    long getRoleCount(@Param("role") String role);
 }
